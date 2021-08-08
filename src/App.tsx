8c9cac8ce466f86bf6c51c18303ec00d05fec7a8
George Shao <GeorgeShao123@gmail.com>
2021-08-08 18:21:58 -0400

    feat: setup React Router + basic app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+import Booked from "./pages/Booked"
+import Explore from "./pages/Explore"
+import Landing from "./pages/Landing"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/explore" component={Explore} />
+        <Route exact path="/booked" component={Booked} />
+      </Router>
     </div>
   );
 }
