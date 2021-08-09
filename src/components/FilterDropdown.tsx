467caa7503daac8c3f4461eae09f6cd8a638f7b4
George Shao <GeorgeShao123@gmail.com>
2021-08-09 01:23:48 -0400

    feat: created filter dropdowns + filter state management on explore page

---
+++ src/components/FilterDropdown.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Menu, Dropdown, Button } from "antd";
+import ArrowDropDownOutlined from '@material-ui/icons/ArrowDropDownOutlined';
+
+function FilterDropdown(props: any) {
+  function handleMenuClick(e: any) {
+    props.onChange(e.key);
+  }
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      {props.items.map((item: string, index: number) => (
+        <Menu.Item key={index} icon={props.icon}>
+          {item}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <div>
+      <Dropdown overlay={menu}>
+        <Button>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "stretch",
+            }}
+          >
+            <div>{props.icon}</div>
+            <div>
+              {props.value ? props.items[props.value] : props.category}{" "}
+            </div>
+            <div>
+              <ArrowDropDownOutlined />
+            </div>
+          </div>
+        </Button>
+      </Dropdown>
+    </div>
+  );
+}
+
+export default FilterDropdown;
---
+++ src/components/FilterDropdownGroup.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import FilterDropdown from "./FilterDropdown";
+import HomeOutlined from "@material-ui/icons/HomeOutlined";
+import PersonOutlined from "@material-ui/icons/PersonOutlined";
+import { DatePicker, TimePicker, Space, Button } from "antd";
+import moment from "moment";
+
+function FilterDropdownGroup(props: any) {
+  function onRoomChange(room: string) {
+    props.onRoomChange(room);
+  }
+
+  function onDateChange(date: any, dateString: string) {
+    props.onDateChange(date);
+  }
+
+  function onTimeChange(time: any, timeString: string) {
+    props.onTimeChange(time);
+  }
+
+  function onCapacityChange(room: string) {
+    props.onCapacityChange(room);
+  }
+
+  function clearAllFilters() {
+    props.onRoomChange("");
+    props.onDateChange("");
+    props.onTimeChange("");
+    props.onCapacityChange("");
+  }
+
+  return (
+    <div>
+      <Space direction="horizontal">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <Button
+            type="link"
+            style={{ color: "rgb(0, 0, 0)", cursor: "default" }}
+          >
+            <b>Filter By:</b>
+          </Button>
+          <div style={{ marginRight: "10px" }}>
+            <FilterDropdown
+              category="Room"
+              items={["A", "B", "C", "D", "E"]}
+              icon={<HomeOutlined />}
+              value={props.room}
+              onChange={onRoomChange}
+            />
+          </div>
+          <div style={{ marginRight: "10px" }}>
+            <DatePicker onChange={onDateChange} value={props.date}/>
+          </div>
+          <div style={{ marginRight: "10px" }}>
+            <TimePicker
+              onChange={onTimeChange}
+              defaultOpenValue={moment("00:00:00", "HH:mm:ss")}
+              value={props.time}
+            />
+          </div>
+          <FilterDropdown
+            category="Capacity"
+            items={Array.from({ length: 10 }, (_, i) => i + 1)}
+            icon={<PersonOutlined />}
+            value={props.capacity}
+            onChange={onCapacityChange}
+          />
+          <Button type="text" onClick={clearAllFilters}>
+            Clear All
+          </Button>
+        </div>
+      </Space>
+    </div>
+  );
+}
+
+export default FilterDropdownGroup;
