3439c6d1d09ed90cdfc018cb51c601c72c3b8ce4
George Shao <GeorgeShao123@gmail.com>
2021-08-09 11:23:31 -0400

    feat: created filter dropdowns (#1)
    * feat: created filter dropdowns + filter state management on explore page
    
    * fix: corrected image file & defaultValue TimePicker warnings

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import FilterDropdownGroup from "../components/FilterDropdownGroup";
 
 function Explore() {
+  const [room, setRoom] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [capacity, setCapacity] = useState("");
+
+  useEffect(() => {
+    if (room || date || time || capacity) {
+      console.table({room: room, date: date, time: time, capacity: capacity});
+    } else {
+      console.log("No filters selected");
+    }
+  }, [room, date, time, capacity]);
+
+  function handleRoomChange(updated_room: string) {
+    setRoom(updated_room);
+  }
+
+  function handleDateChange(updated_date: string) {
+    setDate(updated_date);
+  }
+
+  function handleTimeChange(updated_time: string) {
+    setTime(updated_time);
+  }
+
+  function handleCapacityChange(updated_capacity: string) {
+    setCapacity(updated_capacity);
+  }
+
   return (
     <div>
-      e
+      <FilterDropdownGroup
+        room={room}
+        date={date}
+        time={time}
+        capacity={capacity}
+        onRoomChange={handleRoomChange}
+        onDateChange={handleDateChange}
+        onTimeChange={handleTimeChange}
+        onCapacityChange={handleCapacityChange}
+      />
     </div>
-  )
+  );
 }
 
-export default Explore
+export default Explore;
