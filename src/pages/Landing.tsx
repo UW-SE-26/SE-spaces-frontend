4da49a95ec1c10131659190011cef87d7e24e5a2
DjKesu <km67000@yahoo.com>
2021-08-18 11:05:56 +0530

    setup-landing-page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+
+import { Container } from '@material-ui/core';
+import { Button } from 'antd';
+import React from 'react';
+import Navbar from '../components/Navbar';
+import "../styles/landing.css";
 
 function Landing() {
   return (
-    <div>
-      L
+    <Container className="land">
+      <div className="flex-container">
+        <div className="flex-item-left">
+          <section className="write">
+            <p className="waterloo">
+              University of Waterloo
+            </p>
+
+            <h1 className="bold-text">
+              Study Space Booking System for Software Engineering Students.
+            </h1>
+
+            <Button className="viola">Book a Room</Button>
+
+          </section>
+        </div>
+        <div className="flex-item-right">
+          <div className="imustrator">
+          </div>
+        </div>
     </div>
+    </Container>
   )
 }
 
